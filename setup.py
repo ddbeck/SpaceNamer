6fda20ab42bad4542139f09cb918cdbfe8d96ea5
Daniel D. Beck <me@danieldbeck.com>
2014-05-10 20:46:19 -0400

    add click install requirement

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(
     author_email='me@danieldbeck.com',
     packages=['spacenamer'],
     package_data={'spacenamer': ['data/*.json']},
-    requires=['tweepy'],
+    install_requires=['click', 'tweepy'],
 )
