04602ceeadeab6375d8de7e6547f0344a2bdbc3c
Daniel D. Beck <me@danieldbeck.com>
2014-09-05 22:03:43 -0400

    ignore exception for having already followed somebody

--- spacenamer/__main__.py
+++ spacenamer/__main__.py
@@ -2,6 +2,7 @@ import json
 import random
 
 import click
+import tweepy
 
 from spacenamer.followers import Followers
 from spacenamer.publisher import authenticate, generate_status, post
@@ -23,7 +24,13 @@ def publish(keysfile, followersfile, word=None, dryrun=False):
     if word is None and use_followers:
         with followers.consume_unnamed(dryrun=dryrun) as follower:
             if not dryrun:
-                twitter.create_friendship(follower)
+                try:
+                    twitter.create_friendship(follower)
+                except tweepy.error.TweepError as err:
+                    if "u'code': 160" in err.reason:
+                        pass  # if we're already following, that's fine
+                    else:
+                        raise
             post(twitter, generate_status('@{}'.format(follower)),
                  dryrun=dryrun)
     else:
